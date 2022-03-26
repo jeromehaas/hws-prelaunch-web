12d743c45844e8cc85378ed6927052080c815817
Jerome Haas <hello@jeromehaas.dev>
2022-03-26 14:09:21 +0100

    Setup: webpack configs

---
+++ .webpack.config.js
@@ -0,0 +1,32 @@
+iconst path = require('path');
+
+module.exports = {
+	entry: './src/js/main.js',
+	resolve: {
+		extensions: ['.webpack.js', '.js']
+	},
+	mode: 'production',
+	performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+},
+	output: {
+		filename: '[name].build.js',
+		path: path.join(__dirname, 'js'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env']
+					}
+				}
+			}
+		]
+	}
+}
