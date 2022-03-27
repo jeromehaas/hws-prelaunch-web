a699f33bfcbd2839d9a39326c5e15b30cb233525
Jerome Haas <hello@jeromehaas.dev>
2022-03-27 22:12:43 +0200

    Setup: frontend for contact-card

--- src/js/main.js
+++ src/js/main.js
@@ -3,55 +3,51 @@ import { gsap } from "gsap";
 class ContactCard {
 
 	constructor() {
-		this.timing = 15;
-		this.timeline = null;
-		this.elements = {
-			contactCard: {
-				card: document.querySelector('.contact-card__inner'),
-				thick: document.querySelector('.contact-card__thick'),
-				sides: {
-					all: document.querySelectorAll('.contact-card__side'),
-					front: document.querySelector('.contact-card--front'),
-					back: document.querySelector('.contact-card--front'),
+		this.contactCardConfigs = {
+				timing: 15,
+				timeline: null,
+				elements: {
+					card: document.querySelector('.contact-card__inner'),
+					thick: document.querySelector('.contact-card__thick'),
+					sides: {
+						all: document.querySelectorAll('.contact-card__side'),
+						front: document.querySelector('.contact-card--front'),
+						back: document.querySelector('.contact-card--front'),
+					}
 				}
-			}
-		}
+			};
 		this.init();
-	}
+	};
 
 	init() {
-		this.setTimeLine();
-		this.setEventListeners();
-		this.appear();
+		this.contactCard.setTimeLine();
+		this.contactCard.setEventListeners();
+		this.contactCard.appear();
 	}
 
-	appear() {
-		gsap.from('.contact-card__inner', { top: '-100vh', duration: 3, ease: 'power4' })
-	}
-
-	setTimeLine() {
-		this.timeline = gsap.timeline({ repeat: -1 });
-		this.timeline.to(".contact-card__inner", { rotationY: "+=360", duration: this.timing, ease: "linear" });
-	}
-
-	enterFocus() {
-		this.timeline.pause();
-		gsap.to('.contact-card__inner', { rotationY: "0", rotation: "0", scale: 1.25 })
-	}
-	
-	exitFocus() {
-		gsap.to('.contact-card__inner', { rotationY: "0", rotation: "15deg", scale: 1 })
-		this.setTimeLine();
-		this.timeline.resume();
-		console.log('exit');
-	}
-
-
-	setEventListeners() {
-		gsap.utils.toArray(".contact-card").forEach(flip => {
-			flip.addEventListener("mouseenter", event => this.enterFocus());
-			flip.addEventListener("mouseleave", event => this.exitFocus());
-		});
+	contactCard = {
+		appear: () => {
+			gsap.from('.contact-card__inner', { top: '-100vh', duration: 3, ease: 'power4' })
+		},
+		setTimeLine: () => {
+			this.contactCardConfigs.timeline = gsap.timeline({ repeat: -1 });
+			this.contactCardConfigs.timeline.to(".contact-card__inner", { rotationY: "+=360", duration: this.contactCardConfigs.timing, ease: "linear" });
+		},
+		setEventListeners() {
+			gsap.utils.toArray(".contact-card").forEach(card => {
+				card.addEventListener("mouseenter", () => this.enterFocus());
+				card.addEventListener("mouseleave", () => this.exitFocus());
+			});
+		},
+		enterFocus: () => {
+			this.contactCardConfigs.timeline.pause();
+			gsap.to('.contact-card__inner', { rotationY: "0", rotation: "0", scale: 1.25, duration: 1 })
+		},
+		exitFocus: () => {
+			gsap.to('.contact-card__inner', { rotationY: "0", rotation: "15deg", scale: 1, duration: 1 })
+			this.contactCard.setTimeLine();
+			this.contactCardConfigs.timeline.resume();
+		}
 	};
 
 };
